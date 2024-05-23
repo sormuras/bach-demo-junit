51676d773bbf28f93b501477255a30b6f4a956b2
Christian Stein <sormuras@gmail.com>
2024-05-23 12:50:51 +0000

    Improve naming and documentation

--- .bach/src/JUnitRunner.java
+++ .bach/src/JUnitRunner.java
@@ -1,3 +1,4 @@
+// unnamed package
 
 import bach.info.org.junit.JUnit;
 import java.lang.module.ModuleFinder;
@@ -5,15 +6,29 @@ import java.nio.file.Path;
 import run.bach.ModuleResolver;
 import run.bach.ToolFinder;
 
-class Demo {
+/**
+ * An application installing and running JUnit via its modular tool provider.
+ */
+class JUnitRunner {
+
     public static void main(String... args) {
         var lib = Path.of("lib");
+
+        System.out.println("\nResolving JUnit modules into " + lib.toUri() + " ...");
         var resolver = ModuleResolver.ofSingleDirectory(lib, JUnit.MODULES);
         resolver.resolveModule("org.junit.jupiter"); // to write and discover tests
         resolver.resolveModule("org.junit.platform.console"); // to run tests
         resolver.resolveMissingModules();
 
-        var tools = ToolFinder.of(ModuleFinder.of(lib));
+        var modules = ModuleFinder.of(lib);
+        System.out.println("\nModules found in " + lib + " directory:");
+        modules.findAll().stream()
+                .map(mr -> "\t" + mr.descriptor().toNameAndVersion())
+                .sorted()
+                .forEach(System.out::println);
+
+        var tools = ToolFinder.of(modules);
+        System.out.println("\nAbout to run JUnit...");
         tools.get("junit").run(args.length == 0 ? new String[]{"--version"} : args);
     }
 }
--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # bach-demo-junit
 ✅ Bach + JUnit
 
-```
+```shell
 java @junit engines
+```
+```
 | org.junit.platform.console/junit@1.11.0-M2 engines
 
 💚 Thanks for using JUnit! Support its development at https://junit.org/sponsoring
@@ -12,11 +14,14 @@ junit-jupiter (org.junit.jupiter:junit-jupiter-engine:5.11.0-M2)
 
 ## Command History
 
+The following commands were used to configure this project.
+
 ```shell
 sdk install java 22-open
 git submodule add --name run.bach https://github.com/sormuras/run.bach .bach/src/run/bach
 git submodule add --name junit https://github.com/junit-team/bach.info.org.junit .bach/src/bach/info/org/junit
-java .bach/src/Demo.java
-echo ".bach/src/Demo.java" > junit
+edit .bach/src/JUnitRunner.java
+java .bach/src/JUnitRunner.java
+echo ".bach/src/JUnitRunner.java" > junit
 java @junit engines
 ```
